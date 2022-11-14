d252c0340d4cd56d64ffcc39fd96bd1528be31ce
atharv3218 <100109564+atharv3218@users.noreply.github.com>
2022-11-14 13:58:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["James", "John", "Jen", "Jason", "Atharv", "Shreyash", "Larry", "Paula", "Abhishek", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
